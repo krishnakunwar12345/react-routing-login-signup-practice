b3f159f1f57b75de2c221a34b58dee8230e38fe3
krishnakunwar12345 <krishnakunwar0505@gmail.com>
2023-11-09 11:03:53 +0530

    todaychanges

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <div>
+    { /* <Login/> */}
+    <Signup/>
+    </div>    
+  );
+}
+
+export default App;
