8e6dce7a63a1bec8d7ec72d5edbfe83bee7313df
krishnakunwar12345 <krishnakunwar0505@gmail.com>
2023-11-10 20:24:46 +0530

    nice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
+import Postoverview from './pages/Postoverview';
 function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Login/>}></Route>
         <Route exact path="/login" element={<Login/>}></Route>
         <Route exact path="/signup" element={<Signup/>}></Route>
+        <Route exact path="/postoverview" element={<Postoverview/>}></Route>
       </Routes>
     </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ const Navbar = () => {
           <a classNameName="navbar-brand ms-5">
             <img alt="logo" src={logo} height="45px"/>
           </a>
-          <form className="d-flex" role="search">
+          <form className="d-flex ms-5" role="search">
             <input className="searchbox form-control me-2 text-muted" type="search" placeholder="Search"/>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>
-            <a class="nav-link" href="#">Link</a>         
+            <a class="nav-link text-dark fs-5" href="#"><i class="fa-solid fa-house"></i></a>
+            <a class="nav-link text-dark fs-5" href="#"><i class="fa-regular fa-heart"></i></a>
+            <a class="nav-link text-dark fs-5" href="#"><i class="fa-solid fa-circle"></i></a>         
           </form>
         </div>
       </nav>
